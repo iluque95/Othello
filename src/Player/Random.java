3bda0d45725d601e953df409cf2404e3472be5fc
decube83 <Pira2895>
2018-12-06 18:52:34 +0100

    Board omplert

---
+++ src/Player/Random.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Player;
+
+import Logic.Board;
+import Util.Point;
+import java.util.Vector;
+import javafx.util.Pair;
+
+
+/**
+ *
+ * @author heri
+ */
+public class Random {
+    public String name()
+    {
+        return "Random";      
+    }
+    
+    public Pair<Point, Integer> movement(Board t, int color){
+        Vector<Pair<Point, Integer>> list = t.getMovements(color);
+        //Jelp to quitar this shit joder, me deja hacer import por que me dice que ya esta importado, y si no lo import no lo understend pa nah
+        java.util.Random rnd = new java.util.Random();
+        return list.get(rnd.nextInt(list.size()-1));
+    }
+}
