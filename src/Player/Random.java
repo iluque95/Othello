5cc2aac0a70a42ef1e8582bb876e711d070efe5d
decube83 <Pira2895>
2018-12-06 19:45:20 +0100

    random

--- src/Player/Random.java
+++ src/Player/Random.java
@@ -21,11 +21,12 @@ public class Random {
         return "Random";      
     }
     
-    public Pair<Point, Integer> movement(Board t, int color){
+    public Point movement(Board t, int color){
         //Board ha de tener movimientos, los pasamos calculados o los recalculamos en player, los ponemos como atributo de board?
         Vector<Pair<Point, Integer>> list = t.getMovements(color);
         //Jelp to quitar this shit joder, me deja hacer import por que me dice que ya esta importado, y si no lo import no lo understend pa nah
         java.util.Random rnd = new java.util.Random();
-        return list.get(rnd.nextInt(list.size()-1));
+        Pair<Point, Integer> res = list.get(rnd.nextInt(list.size()-1));
+        return res.getKey();
     }
 }
